17c06dc474899fd60fa2ceb076125fd939c3db0c
audubilly <audubilly89@gmail.com>
2021-09-10 12:22:30 +0100

    entity, repository, services,controller packages

---
+++ src/main/java/com/billy/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.billy.department.repository;
+
+import com.billy.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Id;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+}
