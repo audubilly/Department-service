c23af05fe92be42a52e8be9e852927fd81fb60bc
audubilly <audubilly89@gmail.com>
2021-10-01 12:19:51 +0100

    configured rabbitMq into the application for message queues

---
+++ src/main/java/com/billy/department/config/MessageListener.java
@@ -0,0 +1,15 @@
+package com.billy.department.config;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListener {
+
+    @RabbitListener(queues = MQConfig.QUEUE)
+    public void Listener(CustomMessage message){
+        System.out.println(message);
+    }
+}
